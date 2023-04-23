12281c23a5e86d5701428923e1efc05929e34215
Devin Lindsay <devinlindsay@Devins-iMac.local>
2023-04-22 19:36:35 -0700

    modified main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 # This is the main Python file for our backend.
 
+#from newscatcher import Newscatcher, describe_url
+# import Newscatcher as nc
+#import json
+#import newspaper
+import scrapy
 
 
 
