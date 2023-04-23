92b085e8d65705b23104919fde86b1c67c791155
Devin Lindsay <devinlindsay@Devins-iMac.local>
2023-04-22 21:58:20 -0700

    Beta 0.1

--- backend/main.py
+++ backend/main.py
@@ -6,20 +6,19 @@
 #import newspaper
 import feedparser
 import json
-import rss.py
 
 #url for rss wired
-# wired_business_rss = 'https://www.wired.com/feed/category/business/latest/rss'
-# bleeping_computer_rss = 'https://www.bleepingcomputer.com/feed/'
-# krebs_onsecurity_rss = 'https://krebsonsecurity.com/feed/'
-# google_security_rss = 'https://news.google.com/rss/search?q=Cyber+Security'
-# att_security_rss = 'https://cybersecurity.att.com/site/blog-all-rss'
-# schneier_security_rss = 'https://www.schneier.com/feed/atom/'
-# techcrunch_rss = 'https://techcrunch.com/feed/'
-# thn_security_rss = 'https://feeds.feedburner.com/TheHackersNews'
-# theverge_rss = 'https://www.theverge.com/rss/index.xml'
-# nvd_rss = 'https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml'
- 
+wired_business_rss = 'https://www.wired.com/feed/category/business/latest/rss'
+bleeping_computer_rss = 'https://www.bleepingcomputer.com/feed/'
+krebs_onsecurity_rss = 'https://krebsonsecurity.com/feed/'
+google_security_rss = 'https://news.google.com/rss/search?q=Cyber+Security'
+att_security_rss = 'https://cybersecurity.att.com/site/blog-all-rss'
+schneier_security_rss = 'https://www.schneier.com/feed/atom/'
+techcrunch_rss = 'https://techcrunch.com/feed/'
+thn_security_rss = 'https://feeds.feedburner.com/TheHackersNews'
+theverge_rss = 'https://www.theverge.com/rss/index.xml'
+nvd_rss = 'https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml'
+
 
 #parses the rss url
 wired = feedparser.parse(wired_business_rss)
@@ -36,6 +35,6 @@ nvd = feedparser.parse(nvd_rss)
 #take the top 5 entries using arithmetic
 for i in range(5):
     #print('\nWired Title' + str(i) + ' - ' + wired.entries[i].title + ' | ' + wired.entries[i].description)
-    #print('\nBleeping Title' + str(i) + ' - ' + bleeping.entries[i].title + ' | ' + bleeping.entries[i].description)
-    #print('\nKrebs Title' + str(i) + ' - ' + krebs.entries[i].title + ' | ' + krebs.entries[i].description)
+    print('\nBleeping Title' + str(i) + ' - ' + bleeping.entries[i].title + ' | ' + bleeping.entries[i].description)
+    print('\nKrebs Title' + str(i) + ' - ' + krebs.entries[i].title + ' | ' + krebs.entries[i].description)
     print('\nNVD Title' + str(i) + ' - ' + nvd.entries[i].title + ' - ' + nvd.entries[i].description)
